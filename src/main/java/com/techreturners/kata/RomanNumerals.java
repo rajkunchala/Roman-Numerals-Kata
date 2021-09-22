4ca155907556b28341b0b92e1b70b9bfc112357a
raja <kunchala@yahoo.com>
2021-09-23 00:58:16 +0100

    Initial commit

--- src/main/java/com/techreturners/kata/RomanNumerals.java
+++ src/main/java/com/techreturners/kata/RomanNumerals.java
@@ -2,7 +2,32 @@ package com.techreturners.kata;
 
 public class RomanNumerals {
 
-    public String getRomanLiteralForNumber(int number){
-        return "";
+    public String getRomanLiteral(int number) {
+
+        //Validate the input for under 3000 and throw an exception
+
+        String[] onesArray = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] tensArray = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] hundredsArray = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] thousandsArray = {"","M","MM","MMM"};
+        // roman literals stored in arrays
+        //we can retrieve them with index
+
+        //we can divide the input number and do a modulus to get the times
+        //3251
+        // divide by 1000 and we can get 3...we need MMM in roman
+        // divide by 100 and % 10 to get 2 times C
+        // divide by 10 and % 10 to 5 times X, L
+        // modulus by 10 to get ones i.e. I etc
+
+        String result = "";
+
+        result +=thousandsArray[(number/1000)%10];
+        result +=hundredsArray[(number/100)%10];
+        result +=tensArray[(number/10)%10];
+        result += onesArray[number%10];
+
+
+        return result;
     }
 }
